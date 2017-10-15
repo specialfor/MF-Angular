35258ebd94b356ddce72057b36165f25dfd6fa6b
Danylo Golubtsov <Danylo_Golubtsov@epam.com>
2017-10-15 16:18:17 +0300

    delete my-dream-app

--- lab1/Golubtsov/my-dream-app/src/js/test.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', []);
-
-app.controller('controller', function($scope) {
-	$scope.updateValue = function() {
-		$scope.text = $scope.input;
-		$scope.input = "";
-	};
-});
